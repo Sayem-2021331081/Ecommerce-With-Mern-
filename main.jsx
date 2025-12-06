e0433632046d5c10217b45be7cbe510a55dae70a
shahriargalib037 <shahriarsowmik474@gmail.com>
2025-12-06 20:35:43 +0600

    added main.jsx file

---
+++ main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap'
+import './assets/css/main.css'
+import './assets/css/animate.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'react-loading-skeleton/dist/skeleton.css'
+import App from './App.jsx'
+
+axios.defaults.withCredentials = true;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+          <App />
+  </React.StrictMode>
+)
